08da01d4530661a63d5c9266b1deace94d6a766a
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2024-07-08 22:01:16 +0900

    middleware bug fix 401

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,13 @@
-// import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// // Make sure that the `/api/webhooks/(.*)` route is not protected here
-// export default clerkMiddleware({
-//    publicRoutes: ['/', '/api/webhooks/clerk']
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { authMiddleware } from "@clerk/nextjs/server";
-
-export default authMiddleware({
-  publicRoutes: ["/", "/api/clerk"],
-});
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/((?!api/webhooks).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
